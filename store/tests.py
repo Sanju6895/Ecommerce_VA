54cc9c81d8685d8e732e8ceb9fe34a6bc7be2326
Sanjay Gupta <guptasanjay9091@gmail.com>
2022-04-29 19:57:25 +0530

    Cart Functionality Completed with Testing

--- store/tests.py
+++ store/tests.py
@@ -1,6 +1,9 @@
 from http import HTTPStatus
 from unittest import skip
 
+
+from importlib import import_module
+from django.conf import settings
 # This modeule is a wrapper around python unittest package
 #When test, a new db is created which contains testing data
 from django.contrib.auth.models import User
@@ -91,3 +94,16 @@ class TestViewResponse(TestCase):
         self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
+    
+    def test_homepage_html(self):
+        """
+        Example: code validation, search HTML for text
+        """
+        request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
+        response = product_all(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>BookStore</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
